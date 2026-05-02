ffc69a0f67ecf2f1f944eab43d7abe783fc38fc9
alturuca <alturuca@gmail.com>
2026-05-01 21:30:48 -0500

    El Servidor corre corectamente se corrigieron los errores de importacion

--- inventario/serializers.py
+++ inventario/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Usuario, Producto, Factura, DetalleFactura, IngresoProducto, DetalleIngresoProducto
+from inventario.models import Usuario, Producto, Factura, DetalleFactura, IngresoProducto, DetalleIngresoProducto
 from decimal import Decimal
 
 
